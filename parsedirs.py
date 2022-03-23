a6a3b1ece73016961d9c7aca92a90a3f0eae8289
Matthew Williams <m@technovangelist.com>
2022-03-23 16:26:52 -0700

    aasht
    Signed-off-by: Matthew Williams <m@technovangelist.com>

--- parsedirs.py
+++ parsedirs.py
@@ -7,26 +7,27 @@ ignorelist = [x.strip() for x in srcignorelist.split(',')
 docsdirectory = os.environ["INPUT_DOCSDIRECTORY"]
 categoriesresponse = requests.get(
     'https://dash.readme.com/api/v1/categories?perPage=10&page=1', headers={'Authorization': 'Basic ' + os.environ["readmeapikey"]})
+if categoriesresponse.status_code == 200:
+    categories = categoriesresponse.json()
+    print(categories)
 
-print(categoriesresponse)
-
-# curl - -request GET \
-#      - -url 'https://dash.readme.com/api/v1/categories?perPage=10&page=1' \
-#      - -header 'Authorization: Basic R1ZUalN3bEp3RVRkbllWOEdtQ3F1YW00bGROV2FibUE6'
-for (dirpath, dirnames, filenames) in os.walk(docsdirectory):
-    if not any(dirpath.startswith(docsdirectory + "/" + ignore) for ignore in ignorelist):
-        for file in filenames:
-            fullpath = os.path.join(dirpath, file).split('/')[1:]
-            title = fullpath[-1]
-            category = fullpath[0]
-            print('title = ' + title)
-            print('category = ' + category)
-            if len(fullpath) > 2:
-                parent = fullpath[-2]
-                if parent == title.replace('.md', ''):
-                    parent = fullpath[-3]
-                if parent == category:
-                    parent = ""
-                else:
-                    print('parent = ' + parent)
-            print('---')
+    # curl - -request GET \
+    #      - -url 'https://dash.readme.com/api/v1/categories?perPage=10&page=1' \
+    #      - -header 'Authorization: Basic R1ZUalN3bEp3RVRkbllWOEdtQ3F1YW00bGROV2FibUE6'
+    for (dirpath, dirnames, filenames) in os.walk(docsdirectory):
+        if not any(dirpath.startswith(docsdirectory + "/" + ignore) for ignore in ignorelist):
+            for file in filenames:
+                fullpath = os.path.join(dirpath, file).split('/')[1:]
+                title = fullpath[-1]
+                category = fullpath[0]
+                print('title = ' + title)
+                print('category = ' + category)
+                if len(fullpath) > 2:
+                    parent = fullpath[-2]
+                    if parent == title.replace('.md', ''):
+                        parent = fullpath[-3]
+                    if parent == category:
+                        parent = ""
+                    else:
+                        print('parent = ' + parent)
+                print('---')
