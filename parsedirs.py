0a1b45786416fa9c4ae4da2a5e80b024d5ad1dfb
Matthew Williams <m@technovangelist.com>
2022-03-23 15:24:59 -0700

    asht
    Signed-off-by: Matthew Williams <m@technovangelist.com>

--- parsedirs.py
+++ parsedirs.py
@@ -3,20 +3,20 @@ srcignorelist = os.environ["INPUT_IGNORELIST"]
 ignorelist = [x.strip() for x in srcignorelist.split(',')
               if not srcignorelist == '']
 docsdirectory = os.environ["INPUT_DOCSDIRECTORY"]
-print(*ignorelist, sep=':::')
-print('Docs directory: ' + docsdirectory)
+# print(*ignorelist, sep=':::')
+# print('Docs directory: ' + docsdirectory)
 
 for (dirpath, dirnames, filenames) in os.walk(docsdirectory):
     # listoffiles += [os.path.join(dirpath, file) for file in filenames]
-    print('dirpath: ' + dirpath)
+    # print('dirpath: ' + dirpath)
     if not any(dirpath.startswith(docsdirectory + "/" + ignore) for ignore in ignorelist):
-        print('filenames: ' + '::'.join(filenames))
+        # print('filenames: ' + '::'.join(filenames))
         for file in filenames:
-            print('dirpath: ' + dirpath)
-            print('file: ' + file)
+            # print('dirpath: ' + dirpath)
+            # print('file: ' + file)
             fullpath = os.path.join(dirpath, file).split('/')[1:]
 
-            print('fullpath: ' + '::'.join(fullpath))
+            # print('fullpath: ' + '::'.join(fullpath))
             print('category = ' + fullpath[0])
             if len(fullpath) > 2:
                 print('parent = ' + fullpath[-2])
