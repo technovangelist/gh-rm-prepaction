7f7973814d0d878312988760991a4935033747c0
Matthew Williams <m@technovangelist.com>
2022-03-25 11:04:16 -0700

    how can i process a yaml list for ignores
    Signed-off-by: Matthew Williams <m@technovangelist.com>

--- utils.py
+++ utils.py
@@ -15,6 +15,8 @@ def getReadmeAPIKey():
 def getIgnoreList():
     srcignorelist = os.environ["INPUT_IGNORELIST"]
 
+    print("srcignorelist: " + srcignorelist)
+
     ignorelist = [x.strip() for x in srcignorelist.split(',')
                   if not srcignorelist == '']
 
